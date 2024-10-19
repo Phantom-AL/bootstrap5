7ad549dbe6a45ee7c6ed5a64db80a292ecbc0e82
Phantom-AL <den2005den0715@gmail.com>
2024-10-19 16:28:29 +0300

    Upgrade

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import Post
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'myblog/index.html')
+        posts = Post.objects.all().order_by('-created_at')
+        paginator = (Paginator(posts, 3))
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'myblog/index.html', context={
+            'page_obj': page_obj
+        })
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post
+        })
